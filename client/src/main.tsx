04730d16b3ac6f3abfe18c0eab9fc1a4b8eb00f8
Atila Mariano <103537702+atilamariano@users.noreply.github.com>
2023-02-15 17:35:23 -0300

    feat: cleaning app files

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import { createBrowserRouter, RouterProvider, Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
