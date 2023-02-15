4f38074041be87da61f3c175021e9f00ba35706b
Atila Mariano <103537702+atilamariano@users.noreply.github.com>
2023-02-15 17:44:55 -0300

    feat: created paths

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import "./index.css";
 import { createBrowserRouter, RouterProvider, Router } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+      },
+      {
+        path: "/new",
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
