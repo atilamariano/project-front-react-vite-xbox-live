863c809009ce68b572deea6e27a19fcfdbec3b10
Atila Mariano <103537702+atilamariano@users.noreply.github.com>
2023-02-15 22:05:19 -0300

    style: body to navbar

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import "./index.css";
 import { createBrowserRouter, RouterProvider, Router } from 'react-router-dom';
 //Pages
 import Home from './routes/Home';
 import NewPost from './routes/NewPost';
+
+import './index.css';
 
 const router = createBrowserRouter([
   {
